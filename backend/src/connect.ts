08b1c990fa1cc661ef76d1c798567c18b679a569
Federico_Campanozzi <federico.campanozzi@gmail.com>
2023-11-21 10:11:33 +0100

    implement multiple connection (have to test parameter)

--- backend/src/connect.ts
+++ backend/src/connect.ts
@@ -14,18 +14,50 @@ const p = path.resolve(__dirname, `../.env.${envfile}`);
 console.log("Read ", p, " configuration");
 dotenv.config({ path: p });
 
-const connect = async () => {
-  await createConnection({
-    type: 'mysql',
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : -1,
-    host: process.env.HOST ? String(process.env.HOST) : "",
-    database: process.env.DB_NAME ? String(process.env.DB_NAME) : "",
-    username: process.env.DB_USER ? String(process.env.DB_USER) : "",
-    password: process.env.DB_PASSWORD ? String(process.env.DB_PASSWORD) : "",
-    entities: [Client, Banker, Transaction],
-		synchronize: true,
-  });
-};
+let connect = async () => { throw new Error("provide a wrong connection string");  return; };
+
+if(envfile == "mysql") {
+  connect = async () => {
+    await createConnection({
+      type: 'mysql',
+      port: process.env.DB_PORT ? Number(process.env.DB_PORT) : -1,
+      host: process.env.HOST ? String(process.env.HOST) : "",
+      database: process.env.DB_NAME ? String(process.env.DB_NAME) : "",
+      username: process.env.DB_USER ? String(process.env.DB_USER) : "",
+      password: process.env.DB_PASSWORD ? String(process.env.DB_PASSWORD) : "",
+      entities: [Client, Banker, Transaction],
+      synchronize: true,
+    });
+  };
+} else if(envfile == "sqlite"){
+  connect = async () => {
+    await createConnection({
+      type: 'sqlite',
+      database: process.env.DB_PATH ? path.resolve(__dirname, String(process.env.DB_PATH)) : "",
+      entities: [Client, Banker, Transaction],
+      synchronize: true,
+    });
+  };
+} else if(envfile == "postgres"){
+  connect = async () => {
+    await createConnection({
+      type: 'postgres',
+      database: process.env.DB_PATH ? path.resolve(__dirname, String(process.env.DB_PATH)) : "",
+      replication:{
+        master:{
+          port: process.env.DB_PORT ? Number(process.env.DB_PORT) : -1,
+          host: process.env.HOST ? String(process.env.HOST) : "",
+          database: process.env.DB_NAME ? String(process.env.DB_NAME) : "",
+          username: process.env.DB_USER ? String(process.env.DB_USER) : "",
+          password: process.env.DB_PASSWORD ? String(process.env.DB_PASSWORD) : ""
+        },
+        slaves:[]
+      },
+      entities: [Client, Banker, Transaction],
+      synchronize: true,
+    });
+  };
+} 
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : -1;
 
